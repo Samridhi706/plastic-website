c253d523583c36f020f0afaa7e6edb8de622c7c0
Samridhi706 <samridhijindal706@gmail.com>
2025-12-22 13:31:09 +0530

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,17 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://plastic-website.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://plastic-website.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://plastic-website.vercel.app/contact",
+      lastModified: new Date(),
+    },
+    // add more pages here
+  ];
+}
--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    siteUrl: "https://plastic-website.vercel.app",
-    generateRobotsTxt: true,
-    outDir: "./public",
-}
